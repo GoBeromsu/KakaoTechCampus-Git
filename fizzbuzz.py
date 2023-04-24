3f5fdc7f7ad72a55d393588be42971721fc9d7c6
goberomsu <gobeumsu@gmail.com>
2023-04-24 21:35:49 +0900

    feat : Print fizz
    condition : i is multiple of 3.
    otherwise, print i itself

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,15 @@
-def do_fizzbuzz():
-  print('hello')
+def do_fizzbuzz(num):
+	"""
+	3: fizz
+	5: buzz
+	15: fizzbuzz
+	etc: num
+	"""
+	for i in range(1,num+1):
+		if i%3==0:
+			print("fizz")
+		else:
+			print(f"{i}")	  
 
 if __name__=='__main__':
-	do_fizzbuzz()
+	do_fizzbuzz(15)
